faa1b8d510a0857597ebe08262949737c0beb7bb
杨虎元 <idocker@qq.com>
2019-12-31 11:05:36 +0800

    learn gin

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("First one go")
-	fmt.Println("Hello World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
